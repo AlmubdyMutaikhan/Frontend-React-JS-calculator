937375ca2056961c1d6c67d5f7dfc87ba23a4953
Mutaikhan Almubdi <mutaikhan.almubdy@gmail.com>
2021-08-07 13:42:16 +0600

    feature:do operations one by one without '='

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -5,16 +5,9 @@ import KeyLayout from '../keyLayout/keyLayout'
 
 const Calculator = () => {
     /* INIT STATE */
-    // stores number values for input layout
-    const [numberValues] = useState([
-        '1','2','3','4','5','6','7','8','9','0','.','ce'
-    ]);
     // displayValue stores the number displayed on the input screen
     const [displayValue, setDisplayValue] = useState('0');
-    // describes arithmetic operator which is gonna be processed
-    const [operators] = useState([
-        '+','-','x','/','='
-    ]);
+    // describes which operator is currently chosen
     const [selectedOperator, setSelectedOperator] = useState('');
     // stores old value of number operations
     const [storedValue, setStoredValue] = useState('0'); 
@@ -27,7 +20,7 @@ const Calculator = () => {
             let floatDisplayValue = parseFloat(displayValue);
             let floatStoredValue = parseFloat(storedValue); // this value is stored, but never displayed
             let resultValue = 0
-    
+            
             if(selectedOperator === '+') {
                 // do sum
                 resultValue = floatDisplayValue + floatStoredValue;
@@ -71,26 +64,37 @@ const Calculator = () => {
             if(displayValue === '0') {
                 setDisplayValue(value);
             } else {
-                setDisplayValue(displayValue + value);
+                if(selectedOperator) {
+                    setStoredValue(displayValue);
+                    setDisplayValue(value);
+                } else {
+                    setDisplayValue(displayValue + value);
+                }
             }
         }
     }
     
-    const setOperator = (value) => {
-        if(operators.includes(value)) {
-            if(value !== '=') {
-                setStoredValue(displayValue);
-                setDisplayValue('0');
-                setSelectedOperator(value);
+    const setOperator = (operatorValue) => {
+            if(operatorValue !== '=') {
+                // if there is already stored value and user wants do next operation immediately
+                if(storedValue && selectedOperator) {
+                    processCalculation();
+                    setStoredValue('');
+                    setSelectedOperator(operatorValue);
+                } else {
+                    setStoredValue(displayValue);
+                    setDisplayValue('0');
+                    setSelectedOperator(operatorValue);
+                }
             } else {
                 processCalculation();
             }
-        }
     }
 
     const clearDisplay = () => {
         setDisplayValue('0');
         setStoredValue('0');
+        setSelectedOperator('');
     }
 
     return(
@@ -98,8 +102,6 @@ const Calculator = () => {
             <InputDisplay displayValue={displayValue}/>
             <KeyLayout 
                 clearDisplay={clearDisplay}
-                operators={operators}
-                numbers={numberValues}
                 setOperator={setOperator}
                 updateDisplayForNumbers={updateDisplay}
                 processCalculation={processCalculation}
--- src/components/keyLayout/keyLayout.js
+++ src/components/keyLayout/keyLayout.js
@@ -1,14 +1,36 @@
 import './styles/index.css'
+import { useState } from 'react'
 
-const KeyLayout = ({handleKeyPress, operators, numbers, setOperator, updateDisplayForNumbers, clearDisplay}) => {
-    const numberKeys = numbers.map((value, iterator) => (
+const KeyLayout = ({setOperator, updateDisplayForNumbers, clearDisplay}) => {
+     /* INIT STATE */
+    // stores number values for input layout
+    const [numberValues] = useState([
+        '1','2','3','4','5','6','7','8','9','0','.','ce'
+    ]);
+
+    // describes arithmetic operator which is gonna be processed
+    const [operators] = useState([
+        '+','-','x','/','='
+    ]);
+    
+    // DEFINE PROCESS HANDLING FUNCTIONS
+    // checks whether operator value is valid or not
+    const validateOperator = (operatorValue) => {
+        if(operators.includes(operatorValue)) {
+            setOperator(operatorValue);
+        } 
+    }
+
+    // create key layout for number values
+    const numberKeys = numberValues.map((value, iterator) => (
         <div className="key" key={`${value}${iterator}`}  onClick={() => (updateDisplayForNumbers(value))}>
             <p>{value}</p>
         </div>
     ))
-    
+
+    // create key layout for operator values
     const operatorKeys = operators.map((value, iterator) => (
-        <div className="key" key={iterator} onClick={event => (setOperator(event.target.innerHTML))}>
+        <div className="key" key={iterator} onClick={event => (validateOperator(event.target.innerHTML))}>
             <p>{value}</p>
         </div>
     ))
